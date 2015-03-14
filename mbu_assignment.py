1443bd33bf75684c810f7078549e182e7e87adf1
moleface <w.haney@gmail.com>
2015-03-14 15:18:25 +0000

    Automatic commit

--- mbu_assignment.py
+++ mbu_assignment.py
@@ -29,12 +29,61 @@ def main():
     #Remember that the first element (element 0) of sys.argv is the program name.
     print "The name of the script is " + str(sys.argv[0])+"."
     #Remember to convert numbers into strings before printing them.
-    print "You have entered " + str(len(sys.argv[1:])) + " arguments."
-    print "They are: " 
+    print "You have entered " + str(len(sys.argv[1:])) + " numbers"
 
     #This is for loop. We take each element from sys.argv and print it to the consile.
     for i in sys.argv[1:]:
-        print str(i)
+    
+        a = int (i)
+             
+        b = a % 5   
+        print a, " % 5 is", b
+              
+        print a, " % 5 is", a % 5
+  
+        print str(a)
+        if a % 2 == 0:
+            print "This number is even"
+        if a % 2 == 1:
+            print "This number is odd"
+            
+        if a % 3 == 0:
+            print "This number is divisible by three"
+        if a % 3 > 0:
+            print "This number is not divisible by three"
+
+        if a % 4 == 0:
+            print "This number is divisible by four"
+        if a % 4 >= 1:
+            print "This number is not divisible by four"
+            
+        if a % 5 == 0:
+            print "This number is divisible by five"
+        if a % 5 == 1:
+            print "This number is not divisible by five"
+
+        if a % 6 == 0:
+            print "This number is divisible by six"
+        if a % 6 == 1:
+            print "This number is not divisible by six"
+        if a % 7 == 0:
+            print "This number is divisible by seven"
+        if a % 7 == 1:
+            print "This number is not divisible by seven"
+        if a % 8 == 0:
+            print "This number is divisible by eight"
+        if a % 8 == 1:
+            print "This number is not divisible by 8"
+        if a % 9 == 0:
+            print "This number is divisible by nine"
+        if a % 9 == 1:
+            print "This number is not divisible by nine"
+
+        if a % 10 == 0:
+            print "This number is divisible by ten"
+        if a % 10 == 1:
+            print "This number is not divisible by ten"
+            
         print "\n"
 
 if __name__ == '__main__':
